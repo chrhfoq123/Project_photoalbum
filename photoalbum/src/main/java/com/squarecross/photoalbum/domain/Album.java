badbd27865d57df76c75aa516b2e9c74960e25f9
chrhfoq123 <chrhfoq123@naver.com>
2023-03-10 23:43:26 +0900

    Implement Photo Domain class

--- photoalbum/src/main/java/com/squarecross/photoalbum/domain/Album.java
+++ photoalbum/src/main/java/com/squarecross/photoalbum/domain/Album.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "album", schema = "photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "album_id")})
@@ -22,6 +23,9 @@ public class Album {
     private Date createdAt;
 
     public Album(){};
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "album", cascade = CascadeType.ALL)
+    private List<Photo> photos;
 
     public long getAlbumId() {
         return albumId;
